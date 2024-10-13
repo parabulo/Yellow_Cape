4d4ec84b2d7d2d9543d33d10099ecced65e1dc63
Parabulo <ryanffroes04@gmail.com>
2024-10-13 15:59:16 -0300

    Removal: Unecessary camera attribute

--- Scripts/Stage/CameraFollow.cs
+++ Scripts/Stage/CameraFollow.cs
@@ -7,7 +7,6 @@ public class CameraFollow : MonoBehaviour {
     private float smoothTime = 0.25f;
     private Vector3 velocity = new Vector3(0f, 0f, 0f);
     public float followDistance = 5f;
-    public float heightOffset = 2f;
 
     //Player object
     [SerializeField] private Transform target;
@@ -23,8 +22,7 @@ public class CameraFollow : MonoBehaviour {
         //Consegue o ponto que será seguido
         Vector3 midPosition = (target.position + direction.position)/2 + offSet;
         //Posição do jogador para camera não abandonar ele, adicionando offset para não se encontrar na exata mesma posição
-        Vector3 playerPos = target.position + new Vector3(0, heightOffset, 0);
-        midPosition = Vector3.ClampMagnitude(midPosition - playerPos, followDistance) + playerPos;
+        midPosition = Vector3.ClampMagnitude(midPosition - target.position, followDistance) + target.position;
 
         transform.position = Vector3.SmoothDamp(transform.position, midPosition, ref velocity, smoothTime);
     }
